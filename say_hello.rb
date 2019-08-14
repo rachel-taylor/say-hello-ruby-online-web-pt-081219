5fa059cc406209cbca244bd7d150cd6db00a518d
Rachel T <rachmaybe@gmail.com>
2019-08-14 17:45:00 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,9 +1,10 @@
 def say_hello(name="Kent Beck", language="Ruby")
   puts "Hello #{name}!"
+  puts "Hello #{language}!"
 end 
-def language (language="Ruby")
-  puts "Hello #{language} Programmer!"
-end 
+# def language (language="Ruby")
+#   puts "Hello #{language} Programmer!"
+# end 
 # def say_hello(language="Ruby")
 #   puts "Hello #{language} Programmer!"
 # end 
