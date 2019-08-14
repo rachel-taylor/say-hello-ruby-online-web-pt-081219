7a1ca85c4edecc94a2c66de8d35d9d53ee5e3c81
Rachel T <rachmaybe@gmail.com>
2019-08-14 17:38:47 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,1 +1,9 @@
-# Build your say_hello method here
+def say_hello(name="Kent Beck", language="Ruby")
+  puts "Hello #{name}!"
+end 
+def language (language="Ruby")
+  puts "Hello #{language} Programmer!"
+end 
+# def say_hello(language="Ruby")
+#   puts "Hello #{language} Programmer!"
+# end 
